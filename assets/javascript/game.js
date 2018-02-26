187599aed387758bdfa1f75b8080b8e84e1c61e5
Connor Reed <connorreed@connors-mbp-2.home>
2018-02-25 22:25:22 -0500

    updated hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 
 
-var letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+                     'u', 'v', 'w', 'x', 'y', 'z']
 
 var wins = 0;
 var losses = 0;
@@ -14,7 +16,7 @@ document.onkeyup = function(event) {
 
     var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
-    
+
     if (letterChoices.indexOf(userGuess) > -1) {
      
          if (userGuess === computerGuess) {
@@ -22,7 +24,7 @@ document.onkeyup = function(event) {
              guessesLeft = 9;
              pastGuess = [];
         } 
-        if (userGuess !== computerGuess) {
+        if (userGuess != computerGuess) {
              guessesLeft--;
              pastGuess.push(userGuess);
          } 
@@ -39,7 +41,7 @@ document.onkeyup = function(event) {
          '<h3>Wins: ' + wins + '</h3>' +
          '<h3>Losses: ' + losses + '</h3>' +
          '<h3>Guesses Left: ' + guessesLeft + '</h3>' +
-         '<h3>Your Guesses so far: ' + pastGuess.join(" ") + '</h3>';
+         '<h3>Your Guesses so far: ' + pastGuess.join(", ") + '</h3>';
 
          document.getElementById("pGame").innerHTML = html;
 
